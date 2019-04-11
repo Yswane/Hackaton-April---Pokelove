f2f9fa0036d1b628c5ec70dfd00fc4b34f354152
Sandy Das Neves <sandydn78@gmail.com>
2019-04-11 15:28:18 +0200

    add Home page

---
+++ src/components/pageHome.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component{
+    render(){
+        return(
+            <>
+            <title>Pokelove</title>
+            <header></header>
+            <body>
+                <p>test</p>
+
+
+
+                <footer></footer>
+            </body>
+            
+            </>
+        )
+    }
+}
+
+export default Home;
