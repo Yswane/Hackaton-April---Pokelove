b2259dc9ade587aeafe2b5a4c1cf789c8d63c6e5
Elayache Farouk <fa.elayache1@gmail.com>
2019-04-12 00:26:07 +0200

    css des cards

--- src/components/ImageCentral.js
+++ src/components/ImageCentral.js
@@ -11,7 +11,7 @@ class ImageCentral extends Component {
             <img src="https://zupimages.net/up/19/15/8rgj.png" alt="logo"></img>
           </div>
           <img className="ImageCentral" src="https://image.noelshack.com/fichiers/2019/15/4/1554991425-image-pokelove.png" alt="fond d'ecran de pokelove "></img>
-        <img className="contenair-pokeball" src="https://uploads.scratch.mit.edu/users/avatars/1793/2341.png"></img>
+        <img className="contenair-pokeball" src="https://uploads.scratch.mit.edu/users/avatars/1793/2341.png" alt="fond ecran"></img>
         
         </div>
       </>
