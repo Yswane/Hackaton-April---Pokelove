ae4ed485748668e7374e208442b9c5ae6db4a992
Yous <ysouane0@gmail.com>
2019-04-12 00:24:12 +0200

    random done !

--- src/components/ImageCentral.js
+++ src/components/ImageCentral.js
@@ -1,22 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './ImageCentral.css';
 
-class ImageCentral extends Component {
-  render() {
+const ImageCentral = (props) =>  {
     return (
-      <>
-
         <div className="DivImageCentral">
           <div className="logo">
             <img src="https://zupimages.net/up/19/15/8rgj.png" alt="logo"></img>
           </div>
           <img className="ImageCentral" src="https://image.noelshack.com/fichiers/2019/15/4/1554991425-image-pokelove.png" alt="fond d'ecran de pokelove "></img>
-        <img className="contenair-pokeball" src="https://image.noelshack.com/fichiers/2019/15/4/1555012564-pokeball.gif"></img>
-        
+        <img className="contenair-pokeball" onClick={props.handleChangePokemon} src="https://image.noelshack.com/fichiers/2019/15/4/1555012564-pokeball.gif" ></img>
         </div>
-      </>
     )
-  }
+  
 }
 
 
--- src/components/pageHome.js
+++ src/components/pageHome.js
@@ -7,12 +7,22 @@ import pokes from './all.json'
 class Home extends Component {
     state = {
         pokemon1: pokes,
-        pokemon2: pokes
+        pokemon2: pokes,
+        randomPoke1: 1,
+        randomPoke2: 2,
     }
 
 
-    getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
+    getRandomInt = () => {
+        const poke1 = Math.floor(Math.random() * Math.floor(151));
+        const poke2 = Math.floor(Math.random() * Math.floor(151));
+        if (poke1 === poke2) {
+            poke2 += 1
+        }
+        this.setState({
+            randomPoke1: poke1,
+            randomPoke2: poke2
+        })
     }
 
 
@@ -21,10 +31,10 @@ class Home extends Component {
         console.log(pokes)
         return (
             <>
-                <title>Pokelove</title>
-                <ImageCentral />
+                <title >Pokelove</title>
+                <ImageCentral handleChangePokemon={this.getRandomInt} />
                 <body>
-                    <Card poke1={this.state.pokemon1[this.getRandomInt(151)]} poke2={this.state.pokemon2[this.getRandomInt(151)]} />
+                    <Card poke1={this.state.pokemon1[this.state.randomPoke1]} poke2={this.state.pokemon2[this.state.randomPoke2]}/>
                 </body>
             </>
         )
